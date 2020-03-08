20559768288bacf5213d8a54f3a5825f917247c6
xituoche <46586983+xituoche@users.noreply.github.com>
2020-03-08 14:46:35 -0700

    Add files via upload
    gravity & slow

---
+++ Assets/Scripts/Gravity.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour
+{
+    public GameObject[] obj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void enable()
+    {
+        // Debug.Log("test");
+        obj = GameObject.FindGameObjectsWithTag("star");
+        foreach(GameObject dropstar in obj){
+            Rigidbody2D rig = (Rigidbody2D) dropstar.GetComponent(typeof(Rigidbody2D));
+            rig.gravityScale = 1;
+        }
+    }
+    
+}
